5cf65dc908883d96066e94f14a63aada6f5d508e
Alexandre  Roux <aroux@softbankrobotics.com>
2022-02-09 17:02:52 +0100

    Go to Home page when clicking on logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-    return (
-            <div className="header-container">
-                <div className="header">
-                    <img className="header-logo"
-                         src="https://lereacteur-vinted.netlify.app/static/media/logo.3dcf8b02.png"
-                         alt="vinted"/>
-                    <button className="white-button">S'inscrire</button>
-                    <button className="white-button">Se connecter</button>
-                    <button className="green-button">Vends tes articles</button>
-                </div>
-            </div>
-    );
+  return (
+    <div className="header-container">
+      <div className="header">
+        <Link to="/">
+          <img
+            className="header-logo"
+            src="https://lereacteur-vinted.netlify.app/static/media/logo.3dcf8b02.png"
+            alt="vinted"
+          />
+        </Link>
+        <button className="white-button">S'inscrire</button>
+        <button className="white-button">Se connecter</button>
+        <button className="green-button">Vends tes articles</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
