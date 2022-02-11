5288bd5218d6bd0b38427045dc17f0801552cf44
Alexandre  Roux <aroux@softbankrobotics.com>
2022-02-11 13:21:38 +0100

    Remove header-container

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,44 +12,42 @@ const Header = () => {
   };
 
   return (
-    <div className="header-container">
-      <div className="header">
-        <Link to="/">
-          <img
-            className="header-logo"
-            src="https://lereacteur-vinted.netlify.app/static/media/logo.3dcf8b02.png"
-            alt="vinted"
-          />
-        </Link>
-        <div className="search-container">
-          <FontAwesomeIcon
-            className="search-input-icon"
-            icon="magnifying-glass"
-          />
-          <input
-            className="search-input"
-            type="text"
-            placeholder="Recherche des articles"
-          />
-        </div>
-        <div className="register-login">
-          {token ? (
-            <button className="red-button" onClick={handleLogoutClick}>
-              Se déconnecter
-            </button>
-          ) : (
-            <>
-              <Link to="/signup" style={{ textDecoration: "none" }}>
-                <button className="white-button">S'inscrire</button>
-              </Link>
-              <Link to="/login" style={{ textDecoration: "none" }}>
-                <button className="white-button">Se connecter</button>
-              </Link>
-            </>
-          )}
-        </div>
-        <button className="green-button">Vends tes articles</button>
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header-logo"
+          src="https://lereacteur-vinted.netlify.app/static/media/logo.3dcf8b02.png"
+          alt="vinted"
+        />
+      </Link>
+      <div className="search-container">
+        <FontAwesomeIcon
+          className="search-input-icon"
+          icon="magnifying-glass"
+        />
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Recherche des articles"
+        />
       </div>
+      <div className="register-login">
+        {token ? (
+          <button className="red-button" onClick={handleLogoutClick}>
+            Se déconnecter
+          </button>
+        ) : (
+          <>
+            <Link to="/signup" style={{ textDecoration: "none" }}>
+              <button className="white-button">S'inscrire</button>
+            </Link>
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <button className="white-button">Se connecter</button>
+            </Link>
+          </>
+        )}
+      </div>
+      <button className="green-button">Vends tes articles</button>
     </div>
   );
 };
